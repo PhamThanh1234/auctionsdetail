3dd9a25fa654d4213cad3fada51414b169679a90
PhamThanh1234 <88567002+PhamThanh1234@users.noreply.github.com>
2024-07-12 11:16:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Footer from './components/footer/Footer';
-import EditProfile from './components/EditProfile/EditProfile';
+
 import Header from './components/header/Header';
 
 function App() {
