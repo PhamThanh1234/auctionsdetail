a2854f12a5f230f2de53675d21d304f529b6edc1
PhamThanh1234 <88567002+PhamThanh1234@users.noreply.github.com>
2024-07-16 11:21:57 +0700

    up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Footer from './components/footer/Footer';
 
 import Header from './components/header/Header';
-
+import auctionDetail from './components/auctionDetail/AuctionDetail';
 
 
 function App() {
   return (
+    
     <>
     <Header />
-    <auctionDetail/>
+    <auctionDetail.App />
     <Footer />
     </>
   );
